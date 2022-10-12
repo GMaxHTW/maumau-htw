67fd9f5445e0da2ae8571d46d9225782ca92d140
Ana-Maria Garlau <garlauanamaria@gmail.com>
2022-10-12 17:49:12 +0200

    init

---
+++ src/test/java/com/htw/kbe/maumau/MauMauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htw.kbe.maumau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauMauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
